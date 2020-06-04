0f1f15ac143fb84982ec18eebb90afe125eae475
unknown <dagisky@gmail.com>
2020-06-04 08:14:42 +0300

    Documentation: Doctype and comment for Stack_Array.java

--- Data Structures/Stack_Array.java
+++ Data Structures/Stack_Array.java
@@ -1,6 +1,12 @@
-
+/*
+ * Simple implementation of stack using arrays
+ * Author: Dagmawi Moges
+ * */
 
 class Stack<T>{
+	/*
+	 * This is List implementatio of Stack
+	 * */
 	private int top;
 	private Object[] list;
 	Stack(){
@@ -8,14 +14,25 @@ class Stack<T>{
 		this.list = new Object[10];
 	}
 	private int size() {
+		/*
+		 * Input: None
+		 * Output: (int) total length of the stack*/
 		return this.list.length;		
 	}
 	
 	public int length() {
+		/*
+		 * Input: None
+		 * Output: (int) curtent length of the stack*/
 		return this.top+1;		
 	}
 	
 	public T pop() {
+		/*
+		 * Input: None
+		 * Output: <T> returns single element from the top of the stack
+		 * if the stack is empty returns null, Note this reduces the length of the stack
+		 * */
 		if(this.top != -1) {
 			T ele = (T)this.list[this.top];
 			this.top--;
@@ -25,6 +42,12 @@ class Stack<T>{
 		}
 	}
 	public T peek() {
+		/*
+		 * Input: None
+		 * Output: <T> returns single element from the top of the stack
+		 * returns null of the stack is empty
+		 * Note this dose not reduce the length of the stack
+		 * */
 		if(this.top != -1) {
 			T ele = (T)this.list[this.top];
 			return (T)ele;
@@ -33,6 +56,11 @@ class Stack<T>{
 		}
 	}
 	private void expand() {
+		/*
+		 * Input: None
+		 * Output: None
+		 * expands the size of the stack twice fold, if the stack is full
+		 * */
 		Object[] list = new Object[this.size()*2];
 		for(int i = 0; i < this.top; i++) {
 			list[i] = this.list[i];
@@ -40,6 +68,11 @@ class Stack<T>{
 		this.list = list;
 	}
 	public void push(T e) {
+		/*
+		 * Input: generic type <T>
+		 * Output: None 
+		 * Add single element to the stack
+		 * */
 		if (this.top == this.size()-1)
 			this.expand();		
 		this.list[this.top+1] = e; 
